4f8347cc7aad075189d788c4a07aad4f3690fbd3
Crtique <robinsoncar345@gmail.com>
2026-03-22 21:21:10 -0400

    Many changes

--- ClinicProject2/Assets/Scripts/PlayerControl.cs
+++ ClinicProject2/Assets/Scripts/PlayerControl.cs
@@ -1,16 +1,25 @@
+using System.Diagnostics.Contracts;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
-    // -- Declare Variables --
+    [Header("Movement Checks")]
     [SerializeField] float speed = 100f;
     [SerializeField] float movement = 10f;
+
+    [Header("Jump Checks")]
     [SerializeField] float jumpHeight = 4f;
     [SerializeField] float gravity = -9.81f;
 
-    [Header("Checks")]
+    [Header("Crounch Checks")]
+    [SerializeField] float originalHeight;
+    [SerializeField] float crouchHeight = 2f;
+    [SerializeField] Vector3 originalCenter;
+
+    [Header("Ground Checks")]
     [SerializeField] Transform groundCheck;
     [SerializeField] float groundDistance = 0.4f;
     [SerializeField] LayerMask groundMask;
@@ -18,22 +27,29 @@ public class PlayerControl : MonoBehaviour
     [Header("Controller")]
     [SerializeField] CharacterController controller;
 
+    // -- Declare Variables --
     private float leftBound = -4.4f;
     private float rightBound = 4.4f;
 
     Vector3 velocity;
+
     bool isGrounded;
+    public bool isCrouching;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        controller = GetComponent<CharacterController>();
 
+        originalHeight = controller.height;
+        originalCenter = controller.center;
     }
 
     void Update()
     {
         PlayerMove();
         Jump();
+        Crouch();
     }
 
     // -- Player Movement Function -- //
@@ -69,13 +85,28 @@ public class PlayerControl : MonoBehaviour
         //-- Apply the Velocity when gravity happens --
         controller.Move(velocity * Time.deltaTime);
     }
-
     // -- Player Jump Function -- //
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.W) && isGrounded || Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+    // Make the player "Slide"
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            isCrouching = true;
+            controller.height = crouchHeight;
+            controller.center = new Vector3(originalCenter.x, crouchHeight / 2, originalCenter.z);        
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            isCrouching = false;
+            controller.center = originalCenter;
+            controller.height = originalHeight;
         }
     }
 }
